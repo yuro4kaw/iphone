67762a75724e43da4a7562139341e29a8e43490c
Yurii Matvii <tahkict.yup4ik@gmail.com>
2024-05-08 01:10:50 +0300

    refactor: smooth scroll

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,15 +7,15 @@ const Navbar = () => {
   return (
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center sticky top-0 bg-transparent backdrop-blur-md z-10">
       <nav className="flex w-full screen-max-width">
-        <Image src={appleImg} alt="Apple" width={14} height={18} />
+        <Image className="cursor-pointer" src={appleImg} alt="Apple" width={14} height={18} />
         <div className="flex flex-1 justify-center max-sm:hidden">
           {navLists.map((nav) => (
             <div className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all" key={nav}>{nav}</div>
           ))}
         </div>
         <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
-          <Image src={searchImg} alt="search" width={18} height={18} />
-          <Image src={bagImg} alt="bag" width={18} height={18} />
+          <Image className="cursor-pointer" src={searchImg} alt="search" width={18} height={18} />
+          <Image className="cursor-pointer" src={bagImg} alt="bag" width={18} height={18} />
         </div>
       </nav>
     </header>
